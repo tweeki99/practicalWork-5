1dceb74dd82a8667d7013ed56ebacd01b064de0e
tweeki99 <danil.shipilov.99@gmail.com>
2019-01-15 18:44:35 +0600

    pw5

---
+++ pw5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw5
+{
+    class Program
+    {
+        public static void GenerateError()
+        {
+            CheckEmpty("");
+        }
+        public static void CheckEmpty(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentNullException();
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                GenerateError();
+            }
+            catch (ArgumentNullException excrption)
+            {
+                Console.WriteLine(excrption.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
